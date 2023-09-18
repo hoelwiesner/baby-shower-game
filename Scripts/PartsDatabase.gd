4f232022821541fc79c821c30652fb6edcfdf293
hoelwiesner <hoelwiesner@gmail.com>
2023-09-18 19:17:34 -0400

    first commit

---
+++ Scripts/PartsDatabase.gd
@@ -0,0 +1,33 @@
+const HAIR_LIST = {
+	
+	0: ["Hair", "res://Assets/Parts/Hair/z-default_hair.PNG"],
+	1: ["Charles le Lion", "res://Assets/Parts/Hair/Charles.PNG"],
+	2: ["Train Conductor Sara", "res://Assets/Parts/Hair/Sarah.PNG"],
+}
+
+const EYES_LIST = {
+	
+	0: ["Eyes", "res://Assets/Parts/Eyes/999_eyes.PNG"],
+	1: ["Hoël", "res://Assets/Parts/Eyes/002_eyes.PNG"],
+	2: ["Sarah", "res://Assets/Parts/Eyes/001_eyes.PNG"],
+}
+
+const NOSE_LIST = {
+	0: ["Nose", "res://Assets/Parts/Nose/z-default_nose.PNG"],
+	1: ["Sarah", "res://Assets/Parts/Nose/IMG_1018.PNG"],
+	2: ["Mollenkopf Mustache", "res://Assets/Parts/Nose/IMG_1019.PNG"],
+	3: ["Hoël", "res://Assets/Parts/Nose/IMG_1020.PNG"]
+}
+
+
+const MOUTH_LIST = {
+	0: ["Mouth", "res://Assets/Parts/Mouth/z-default_mouth.PNG"],
+	1: ["Hoël", "res://Assets/Parts/Mouth/IMG_1022.PNG"],
+	2: ["Sarah", "res://Assets/Parts/Mouth/IMG_1021.PNG"],
+}
+
+const OUTFIT_LIST = {
+	0: ["Outfit", "res://Assets/Parts/Outfit/z-default_outfit.PNG"],
+	1: ["Pullover Ourson", "res://Assets/Parts/Outfit/IMG_1024.PNG"],
+	2: ["Leopard Prints", "res://Assets/Parts/Outfit/IMG_1023.PNG"],
+}
